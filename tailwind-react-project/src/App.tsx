b0ded9342587f8b5c05cc479783f6550af1d554f
Valeria <lerik13@gmail.com>
2026-01-16 16:56:11 -0700

    tailwind-react-project: change Theme: Dark or Light

--- tailwind-react-project/src/App.tsx
+++ tailwind-react-project/src/App.tsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 
 function App() {
   const [open, setOpen] = useState<boolean>(false)
+  const [theme, setTheme] = useState('light')
 
   return (
-    <div className='bg-slate-50 min-h-screen text-slate-900'>
+    <div
+      className={`${theme} min-h-screen text-slate-900 dark:bg-slate-900 dark:text-white`}
+    >
       {/* Navbar */}
-      <div className='flex items-center justify-between bg-slate-900 text-white p-4'>
+      <div className='flex items-center justify-between text-slate-900 dark:bg-slate-900 dark:text-white p-4'>
         <div className='font-bold'>Logo</div>
 
         {/* Desktop Nav */}
@@ -14,6 +17,14 @@ function App() {
           <span>Home</span>
           <span>About</span>
           <span>Contact</span>
+          <button
+            className='text-xl cursor-pointer'
+            onClick={() =>
+              setTheme((prev) => (prev === 'light' ? 'dark' : 'light'))
+            }
+          >
+            {theme === 'light' ? '🌙' : '🌞'}
+          </button>
         </div>
 
         <button
@@ -26,14 +37,22 @@ function App() {
 
       {/* Mobile Nav */}
       {open && (
-        <div className='flex flex-col items-center gap-2 bg-slate-900 text-white p-4 sm:hidden'>
+        <div className='flex flex-col items-center gap-2 p-4 sm:hidden text-slate-900 dark:bg-slate-900 dark:text-white'>
           <span>Home</span>
           <span>About</span>
           <span>Contact</span>
+          <button
+            className='text-xl cursor-pointer sm:hidden'
+            onClick={() =>
+              setTheme((prev) => (prev === 'light' ? 'dark' : 'light'))
+            }
+          >
+            {theme === 'light' ? '🌙' : '🌞'}
+          </button>
         </div>
       )}
 
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 text-white text-center p-6 gap-6 text-2xl sm:text-lg'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 text-center p-6 gap-6 text-2xl sm:text-lg text-slate-900 dark:bg-slate-900 text-white'>
         <div className='bg-slate-500 p-4 rounded hover: bg-slate-600 hover:scale-110 transition-all duration-300'>
           Feature One
         </div>
